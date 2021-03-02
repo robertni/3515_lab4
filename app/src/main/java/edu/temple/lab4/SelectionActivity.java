0d3294a124763edf637f08252c174d944015dd4d
Robert Ni <robertni@outlook.fr>
2021-03-02 13:28:21 -0500

    pass data for display activity

--- app/src/main/java/edu/temple/lab4/SelectionActivity.java
+++ app/src/main/java/edu/temple/lab4/SelectionActivity.java
@@ -2,6 +2,7 @@ package edu.temple.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 
 public class SelectionActivity extends AppCompatActivity {
 
+    private final String title = "Item Selection";
     Spinner spinner;
     ImageView imageView;
     int[] dogImages;
@@ -20,6 +22,9 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set title of the activity
+        setTitle(title);
 
         // retrieve views from xml
         spinner = findViewById(R.id.spinner);
@@ -42,7 +47,18 @@ public class SelectionActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (position > 0) {
-                    showPicture(position - 1);
+                    //showPicture(position - 1);
+
+                    // declare new intent
+                    Intent intent = new Intent(SelectionActivity.this, DisplayActivity.class);
+
+                    // pass data to intent
+                    intent.putExtra("dogArray", dogArray);
+                    intent.putExtra("dogImages", dogImages);
+                    intent.putExtra("position", position - 1);
+
+                    // start the activity
+                    startActivity(intent);
                 }
             }
 

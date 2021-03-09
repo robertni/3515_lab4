8fc4d5af569c0c81edc0887a9202e02ceb6cd1dc
Robert Ni <robertni@outlook.fr>
2021-03-09 12:36:18 -0500

    changed DisplayActivity to reflect changes with grid view

--- app/src/main/java/edu/temple/lab4/DisplayActivity.java
+++ app/src/main/java/edu/temple/lab4/DisplayActivity.java
@@ -1,53 +1,36 @@
 package edu.temple.lab4;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class DisplayActivity extends AppCompatActivity {
 
     private final String title = "Display";
 
-    ArrayList<String> dogArray;
-    int[] dogImages;
-    int position;
-
-    ImageView displayImage;
-    TextView textView;
+    TextView text;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
+        text = findViewById(R.id.textView);
+        image = findViewById(R.id.displayImage);
+
         // set title of activity
         setTitle(title);
-
-        // retrieve views from second layout
-        displayImage = findViewById(R.id.displayImage);
-        textView = findViewById(R.id.textView);
 
         // get data from intent
         Intent intent = getIntent();
 
-        // get ArrayList<String> from intent
-        dogArray = (ArrayList<String>)intent.getSerializableExtra("dogArray");
-
-        // get images from intent
-        dogImages = intent.getIntArrayExtra("dogImages");
-
-        // get position from intent
-        position = intent.getIntExtra("position", 0);
-
         // display the data in view
-        displayImage.setImageResource(dogImages[position]);
-        textView.setText(dogArray.get(position));
-        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+        text.setText(intent.getStringExtra("name"));
+        image.setImageResource(intent.getIntExtra("image", R.drawable.ic_launcher_foreground));
     }
 }
